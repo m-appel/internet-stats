006e131f4422f9b88f14b905df3aca25ed768310
Malte Appel <malte@iij-ii.co.jp>
2021-11-05 10:02:48 +0900

    Add script to fetch AS names file from RIPE

---
+++ downloaders/get_as_names.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -euo pipefail
+
+URL=https://ftp.ripe.net/ripe/asnames/asn.txt
+
+TMP=$(mktemp)
+trap "rm -f $TMP" EXIT
+SCRIPT_DIR=$(dirname "$0")
+OUTPUT_DIR="$SCRIPT_DIR/../raw/atlas"
+OUTPUT_SYMLINK="$OUTPUT_DIR/latest-asn-names.txt"
+OUTPUT_FILE_NAME=$(date --utc +%Y%m%d-asn-names.txt)
+OUTPUT_FILE="$OUTPUT_DIR/$OUTPUT_FILE_NAME"
+
+if [ ! -d "$OUTPUT_DIR" ]
+then
+    echo "Error: Failed to find output directory: $OUTPUT_DIR"
+    exit 1
+fi
+
+echo "Downloading $URL"
+curl "$URL" -o "$TMP"
+
+if [ -f "$OUTPUT_SYMLINK" ]
+then
+    echo "Comparing with existing file..."
+    if diff "$TMP" "$OUTPUT_SYMLINK"
+    then
+        echo "Existing file is already up to date."
+        exit 0
+    fi
+    rm "$OUTPUT_SYMLINK"
+fi
+
+mv "$TMP" "$OUTPUT_FILE"
+ln -s "$OUTPUT_FILE_NAME" "$OUTPUT_SYMLINK"
