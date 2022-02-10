3a7457e585a33b89b9d24b658ba4ddf82db0fd10
Malte Appel <malte@iij-ii.co.jp>
2022-02-10 14:25:40 +0900

    Add IPv6 option to group_probes_by_as.py

--- group_probes_by_as.py
+++ group_probes_by_as.py
@@ -8,12 +8,16 @@ DEFAULT_INPUT = 'raw/atlas/latest-probes.msgpack.bz2'
 OUTPUT_SUFFIX = '-by-as'
 
 
-def group_by_as(data: list) -> list:
+def group_by_as(data: list, ipv6: bool) -> list:
+    if ipv6:
+        key = 'asn_v6'
+    else:
+        key = 'asn_v4'
     as_probe_map = defaultdict(int)
     for probe in data:
-        if 'asn_v4' not in probe or not probe['asn_v4']:
+        if key not in probe or not probe[key]:
             continue
-        as_probe_map[probe['asn_v4']] += 1
+        as_probe_map[probe[key]] += 1
     if not as_probe_map:
         return list()
     return [('as', 'probe_count')] + \
@@ -21,21 +25,28 @@ def group_by_as(data: list) -> list:
 
 
 def main() -> None:
+    global OUTPUT_SUFFIX
     log_format = '%(asctime)s %(processName)s %(message)s'
     logging.basicConfig(
         format=log_format,
         level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input',
-                        help='Use this raw dump instead of latest',
+                        help='use this raw dump instead of latest',
                         default=DEFAULT_INPUT)
     parser.add_argument('-o', '--output',
-                        help='Manually specify output file')
+                        help=',anually specify output file')
+    parser.add_argument('--ipv6', action='store_true',
+                         help='use IPv6')
     args = parser.parse_args()
+
+    ipv6 = args.ipv6
+    if ipv6:
+        OUTPUT_SUFFIX += '-v6'
     file = MsgpackFileHandler(input_=args.input, output=args.output,
                               output_name_suffix=OUTPUT_SUFFIX)
     data = file.read()
-    lines = group_by_as(data)
+    lines = group_by_as(data, ipv6)
     file.write(lines)
 
 
